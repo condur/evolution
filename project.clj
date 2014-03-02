6daa8fbc27362d154eb560db58d6250b418aeba9
Dima Condur <kdumitru@gmail.com>
2014-03-02 13:38:07 -0700

    Update Clojure version to "1.6.0-beta2"

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0-alpha3"]
+  :dependencies [[org.clojure/clojure "1.6.0-beta2"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [org.clojure/math.numeric-tower "0.0.3"]
                  [org.clojure/algo.generic "0.1.1"]
